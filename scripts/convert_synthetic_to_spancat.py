6dc9ecb0a23a5a6f179503d54396af49ecba00ef
irvallensar <irvallenmikha@gmail.com>
2026-06-06 00:36:45 +0900

    change to data/synthetic_zero_shot.jsonl
    for doing zero-shot properly this time

--- scripts/convert_synthetic_to_spancat.py
+++ scripts/convert_synthetic_to_spancat.py
@@ -35,3 +35,4 @@ def jsonl_to_spancat(input_jsonl, output_spacy):
     print(f"Successfully compiled {success} synthetic sentences directly to {output_spacy}.")
 
 jsonl_to_spancat('data/synthetic_zero_shot.jsonl', 'data/synthetic_zero_shot.spacy')
+
